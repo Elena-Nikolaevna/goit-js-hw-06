8044a919e9c6855556e0c70e546b5da699c0afcc
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-07-07 01:15:32 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,19 +1,29 @@
-/* Напиши скрипт управления формой логина.
 
-<form class="login-form">
-  <label>
-    Email
-    <input type="email" name="email" />
-  </label>
-  <label>
-    Password
-    <input type="password" name="password" />
-  </label>
-  <button type="submit">Login</button>
-</form>
-Обработка отправки формы form.login-form должна быть по событию submit.
-При отправке формы страница не должна перезагружаться.
-Если в форме есть незаполненные поля, выводи alert с предупреждением о том, что все поля должны быть заполнены.
-Если пользователь заполнил все поля и отправил форму, собери значения полей в обьект, где имя поля будет именем свойства, а значение поля - значением свойства. Для доступа к элементам формы используй свойство elements.
-Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset.
- */
+const form = document.querySelector(".login-form");
+//console.log(form);
+
+form.addEventListener("submit", onFormSubmit); //Блок подписок
+
+function onFormSubmit(event) {
+    //блок обраьотчиков
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+    console.log();(formElements);
+
+    const mail = formElements.email.value;
+    const password = formElements.password.value;
+
+    const formData = {
+      mail: mail,
+      password: password,
+    };
+
+    console.log(formData)
+}
+//+1.Обработка отправки формы form.login-form должна быть по событию submit.
+//+2.При отправке формы страница не должна перезагружаться.
+//3.Если в форме есть незаполненные поля, выводи alert с предупреждением о том, что все поля должны быть заполнены.
+//4.Если пользователь заполнил все поля и отправил форму, собери значения полей в обьект, где имя поля будет именем свойства, а значение поля - значением свойства. Для доступа к элементам формы используй свойство elements.
+//в консоль и очисти значения полей формы методом reset.
+//5.Выведи обьект с введенными данными 
