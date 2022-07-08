1bc50169cce289612f18b9ac15afea1d7fde27d6
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-07-08 22:49:52 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,27 +1,29 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+    "Potatoes",
+    "Mushrooms",
+    "Garlic",
+    "Tomatos",
+    "Herbs",
+    "Condiments",
 ];
-/* В HTML есть пустой список ul#ingredients.
 
-<ul id="ingredients"></ul>
-В JavaScript есть массив строк.
+const ingredientElement = {
+    ingredientsList: document.querySelector("#ingredients"),
+};
 
-const ingredients = [
-  "Potatoes",
-  "Mushrooms",
-  "Garlic",
-  "Tomatos",
-  "Herbs",
-  "Condiments",
-];
-Напиши скрипт, который для каждого элемента массива ingredients:
+const newIngredientList = ingredients.map(nameIngridients => {    
+    const listItem = document.createElement("li");
+    listItem.classList.add("item");
+    listItem.textContent = nameIngridients;
 
-Создаст отдельный элемент <li>. Обзательно используй метод document.createElement().
-Добавит название ингредиента как его текстовое содержимое.
-Добавит элементу класс item.
-После чего вставит все <li> за одну операцию в список ul#ingredients. */
+    return listItem;
+});
+ingredientElement.ingredientsList.append(...newIngredientList);
+//console.log(newIngredientList)
+
+/*
++Создаст отдельный элемент <li>. Обзательно используй метод document.createElement().
++Добавит название ингредиента как его текстовое содержимое.
++Добавит элементу класс item.
++После чего вставит все <li> за одну операцию в список ul#ingredients. 
+*/
