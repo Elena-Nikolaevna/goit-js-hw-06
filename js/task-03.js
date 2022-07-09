c4b07a9d21536531dc643cc7f338ae72fd20a425
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-07-09 22:37:47 +0300

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -1,18 +1,31 @@
 const images = [
-  {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
-  },
-  {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
-  },
-  {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
-  },
+    {
+        url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        alt: "White and Black Long Fur Cat",
+    },
+    {
+        url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+    },
+    {
+        url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        alt: "Group of Horses Running",
+    },
 ];
 
+const newGallery = {
+    gallery: document.querySelector(".gallery"),
+};
+const makeNewImages = images => {
+    return images.map(({ url, alt}) => {
+        return `<li class="gallery__item"><img class="gallery__img" src=${url} alt=${alt}></li>`;
+    });
+};
+const newImages = makeNewImages(images);
+
+newGallery.gallery.insertAdjacentHTML("afterbegin", newImages);
+
+console.log(newImages);
 /* Напиши скрипт для создания галереи изображений по массиву данных. В HTML есть список ul.gallery.
 
 <ul class="gallery"></ul>
