842be327673245bc956dcb9b8fa2d0b31ccad8e5
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-07-11 00:16:51 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,2 +1,10 @@
-/* Напиши скрипт который, при наборе текста в инпуте input#name-input (событие input), подставляет его текущее значение в span#name-output. 
-Если инпут пустой, в спане должна отображаться строка "Anonymous". */
+const refs = {
+    input: document.querySelector("#text"),
+};
+console.log(refs.input);
+
+/* Напиши скрипт который, при наборе текста в инпуте 
+input#name-input (событие input), подставляет его 
+текущее значение в span#name-output. 
+Если инпут пустой, в спане должна отображаться 
+строка "Anonymous". */
