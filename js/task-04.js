b68dcf5d204d6cca1848c23352f63d565216f6b6
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-07-10 22:34:33 +0300

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,10 +1,33 @@
-/* Счетчик состоит из спана и кнопок, которые, при клике, должны увеличивать и уменьшать его значение на единицу.
+const refs = {
+    counter: document.querySelector("#value"),
+    buttonActionIncrement: document.querySelector("[data-action = increment]"),
+    buttonActionDecrement: document.querySelector("[data-action = decrement]"),
+};
+//console.log(refs.counter);
+//console.log(refs.buttonActionIncrement);
+//console.log(refs.buttonActionDecrement);
 
-<div id="counter">
-  <button type="button" data-action="decrement">-1</button>
-  <span id="value">0</span>
-  <button type="button" data-action="increment">+1</button>
-</div>
-Создай переменную counterValue в которой будет храниться текущее значение счетчика и инициализируй её значением 0.
-Добавь слушатели кликов на кнопки, внутри которых увеличивай или уменьшай значение счтетчика.
-Обновляй интерфейс новым значением переменной counterValue */
+refs.buttonActionDecrement.addEventListener(
+    "click",
+    onClickButtonActionDecrement,
+);
+refs.buttonActionIncrement.addEventListener(
+    "click",
+    onClickButtonActionIncrement,
+);
+
+let counterValue = 0;
+
+function onClickButtonActionIncrement() {
+    counterValue += 1;
+
+    refs.counter.textContent = counterValue;
+}
+function onClickButtonActionDecrement() {
+    counterValue -= 1;
+
+    refs.counter.textContent = counterValue;
+}
+//+ Создай переменную counterValue в которой будет храниться текущее значение счетчика и инициализируй её значением 0.
+//+ Добавь слушатели кликов на кнопки, внутри которых увеличивай или уменьшай значение счтетчика.
+//+ Обновляй интерфейс новым значением переменной counterValue
