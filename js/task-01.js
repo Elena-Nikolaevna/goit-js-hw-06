275b5b281d3164b7ca7940b69d1441aedd32519b
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-07-10 22:34:24 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -9,27 +9,13 @@ const navCategories = {
 const categorieslength = navCategories.categories.children.length;
 console.log("Number of categories: ", categorieslength);
 
-
 navCategories.categoriesItems.forEach(item => {
     const categoryName = item.firstElementChild.textContent;
     const categoryCountElements = item.lastElementChild.children.length;
     console.log("Category: ", categoryName);
     console.log("Elements: ", categoryCountElements);
 });
-/* Напиши скрипт который:
 
-  +  1.Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item.
-  +  2.Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль 
-текст заголовка элемента (тега <h2>) и количество элементов в категории (всех вложенных в него <li>).
-В результате, в консоли будут выведены такие сообщения.
-
-Number of categories: 3 +
-
-Category: Animals
-Elements: 4
-
-Category: Products
-Elements: 3
-
-Category: Technologies
-Elements: 5 */
+// +  1.Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item.
+// +  2.Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль
+//текст заголовка элемента (тега <h2>) и количество элементов в категории (всех вложенных в него <li>).
