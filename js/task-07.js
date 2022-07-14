fccb7f96886be4d2da8baa38ad68ef38e0c60e3b
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-07-15 00:39:07 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -3,3 +3,15 @@
 <input id="font-size-control" type="range" min="16" max="96" />
 <br />
 <span id="text">Abracadabra!</span> */
+const refs = {
+    input: document.querySelector("#font-size-control"),
+    span: document.querySelector("#text"),
+};
+console.dir(refs.input);
+//console.log(refs.span);
+
+refs.input.addEventListener("input", onClickResize); // Слушатель принимает( инпут, что сделать)
+function onClickResize(event) {
+    const inputValue = event.currentTarget.value;
+    refs.span.style.fontSize = `${inputValue}px`;
+}
