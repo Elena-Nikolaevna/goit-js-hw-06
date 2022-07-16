91f4ac931853489fff185f0bd6668bd834912221
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-07-16 16:00:29 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,16 +1,20 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-/* Напиши скрипт, который изменяет цвета фона элемента <body> через инлайн стиль при клике на button.change-color и выводит значение цвета в span.color.
 
-<div class="widget">
-  <p>Background color: <span class="color">-</span></p>
-  <button type="button" class="change-color">Change color</button>
-</div>
-Для генерации случайного цвета используй функцию getRandomHexColor.
+const refs = {
+    bodyBackgroundColor: document.body,
+    chengeColorButton: document.querySelector(".change-color"),
+    requiredColor: document.querySelector(".color"),
+};
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-} */
+refs.chengeColorButton.addEventListener("click", onchengeColorButton);
+
+function onchengeColorButton() {
+    let color = getRandomHexColor();
+    refs.bodyBackgroundColor.style.backgroundColor = color;
+    refs.requiredColor.textContent = color;
+}
+//console.log(refs.bodyBackgroundColor);
+//console.log(refs.chengeColorBtn);
+//console.log(refs.requiredColor);
